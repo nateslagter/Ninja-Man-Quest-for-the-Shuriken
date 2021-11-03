ac81023be058405c0534b4eb550ee88464a3a0d0
Nate Slagter <nateslagter@gmail.com>
2021-11-03 14:22:47 -0400

    Add functioning jump to boss ai

--- src/actors/Boss.gd
+++ src/actors/Boss.gd
@@ -8,18 +8,17 @@ const jump_speed := -400
 
 var velocity := Vector2(200,0)
 var switch_velocity = 200
-var jumping := false
-var direction_to_move := "right"
 var moving := false
 	
 
 func _physics_process(delta : float) -> void:
 	velocity.y += gravity * delta
 	if moving:
-		velocity = move_and_slide(velocity,Vector2(0,-1))
+		velocity = move_and_slide(velocity,Vector2(0,0))
 
 
 func _on_MoveTimer_timeout():
+	$JumpTimer.start()
 	switch_velocity *= -1
 	velocity = Vector2(switch_velocity,0)
 	scale.x *= -1
