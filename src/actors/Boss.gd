06ec9770944c002fc84c067a7b5e1493a8fd7051
Nate Slagter <nateslagter@gmail.com>
2021-11-03 00:06:53 -0400

    Move boss every ten seconds

--- src/actors/Boss.gd
+++ src/actors/Boss.gd
@@ -5,12 +5,20 @@ onready var initial_pos := Vector2(position.x,position.y)
 
 var velocity := Vector2(200,0)
 var switch_velocity = 200
+var moving := false
 
 func _physics_process(delta : float) -> void:
-	if position.x > initial_pos.x + 200:
-		velocity = Vector2(-switch_velocity,0)
-		scale.x *= -1
-	elif position.x < initial_pos.x - 200:
-		velocity = Vector2(switch_velocity,0)
-		scale.x *= -1
-	var _ignored = move_and_slide(velocity,Vector2(0,-1))
+	if moving:
+		if position.x > initial_pos.x + 200:
+			velocity = Vector2(-switch_velocity,0)
+			scale.x *= -1
+			moving = false
+		elif position.x < initial_pos.x - 200:
+			velocity = Vector2(switch_velocity,0)
+			scale.x *= -1
+			moving = false
+		var _ignored = move_and_slide(velocity,Vector2(0,-1))
+
+
+func _on_MoveTimer_timeout():
+	moving = true
