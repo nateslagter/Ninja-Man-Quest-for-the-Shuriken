a244150d7ff9aa5f2a9bfb2534225ea6fa4fb7cf
Nate Slagter <nateslagter@gmail.com>
2021-11-04 02:27:26 -0400

    Switch boss ai to counterattack player

--- src/actors/Boss.gd
+++ src/actors/Boss.gd
@@ -10,9 +10,8 @@ onready var initial_pos := Vector2(position.x,position.y)
 
 var velocity := Vector2(150,0)
 var switch_velocity = 150
-var health := 3
+var health := 20
 var movable := true
-var wait_count := 0
 
 
 func _physics_process(delta : float) -> void:
@@ -25,7 +24,6 @@ func _physics_process(delta : float) -> void:
 			velocity = Vector2(switch_velocity, 0)
 			movable = false
 			$WaitTimer.start()
-			$AttackTimer.start()
 		elif position.x < initial_pos.x - 200:
 			scale.x *= -1
 			$AnimationPlayer.play("Idle")
@@ -33,13 +31,14 @@ func _physics_process(delta : float) -> void:
 			velocity = Vector2(switch_velocity, 0)
 			movable = false
 			$WaitTimer.start()
-			$AttackTimer.start()
 		var _ignored = move_and_slide(velocity,Vector2(0,0))
 	
 
 func _on_Hitbox_area_entered(area : Area2D) -> void:
 	if area.name == "SwordHitbox":
 		health -= 1
+		_attack()
+	
 	if health == 0:
 		queue_free()
 
@@ -55,3 +54,8 @@ func _on_AttackTimer_timeout():
 func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
 	if anim_name == "Attack":
 		$AnimationPlayer.play("Idle")
+
+
+func _attack() -> void:
+	$AnimationPlayer.play("Attack")
+	$AttackCooldown.start()
