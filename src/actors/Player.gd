94554df77a2aaabf6eac179f0ce7311c63d14964
Landon Hammond <lthammond@bsu.edu>
2021-11-04 01:58:02 -0400

    Add dodge mechanic

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -9,32 +9,61 @@ const gravity := 700
 var velocity := Vector2()
 var jumping := false
 var attacking := false
+var previously_dodged := false
+var ableToMove := true
+
+var dash_frames := 0
+var dashMultiplier := 2
+
 
 func get_input() -> void:
 	velocity.x = 0
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= run_speed
-		$Sprite.scale.x = -1
-		if jumping == false and attacking == false:
-			$AnimationPlayer.play("Walk")
-	if Input.is_action_pressed("move_right"):
-		velocity.x += run_speed
-		$Sprite.scale.x = 1
-		if jumping == false and attacking == false:
-			$AnimationPlayer.play("Walk")
-	if Input.is_action_just_pressed("jump"):
-		if jumping == false and is_on_floor():
-			jumping = true
-			$AnimationPlayer.play("Jump")
-			velocity.y += jump_speed
-	if Input.is_action_just_pressed("attack"):
-		attacking = true
-		$AnimationPlayer.play("Attack")
-	if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
-		if jumping == false and attacking == false:
-			$AnimationPlayer.play("Idle")
-		
-		
+	if ableToMove: 
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= run_speed
+			$Sprite.scale.x = -1
+			if jumping == false and attacking == false:
+				$AnimationPlayer.play("Walk")
+
+		if Input.is_action_pressed("move_right"):
+			velocity.x += run_speed
+			$Sprite.scale.x = 1
+			if jumping == false and attacking == false:
+				$AnimationPlayer.play("Walk")
+
+		if Input.is_action_just_pressed("jump"):
+			if jumping == false and is_on_floor():
+				jumping = true
+				$AnimationPlayer.play("Jump")
+				velocity.y += jump_speed
+
+		if Input.is_action_just_pressed("attack") and jumping == false:
+			attacking = true
+			$AnimationPlayer.play("Attack")
+
+		if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
+			if jumping == false and attacking == false:
+				$AnimationPlayer.play("Idle")
+
+		if Input.is_action_pressed("dodge_backwards") and !previously_dodged:
+			$AnimationPlayer.play("Dodge")
+			velocity.x = 0
+			ableToMove = false
+			previously_dodged = true
+			dash_frames = 10
+			velocity.x = velocity.x * dashMultiplier
+			$DodgeCooldownTimer.start()
+
+	if dash_frames > 0:
+		dash_frames -= 1
+		if $Sprite.scale.x == 1:
+			velocity.x += -400
+		elif $Sprite.scale.x == -1:
+			velocity.x += 400
+	else:
+		ableToMove = true
+
+
 func _physics_process(delta: float) -> void:
 	get_input()
 	velocity = move_and_slide(velocity,Vector2(0,-1))
@@ -52,13 +81,12 @@ func _on_Area2D_body_entered(body : Node2D) -> void:
 			queue_free()
 			
 
-
 func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
-	if anim_name == "Attack":
+	if anim_name == "Attack" or anim_name == "Dodge":
 		$AnimationPlayer.play("Idle")
 		attacking = false
 
 
-func _on_AutoAttack_body_entered(body):
-	attacking = true
-	$AnimationPlayer.play("Attack")
+func _on_DodgeCooldownTimer_timeout():
+	print("Ready to dodge!")
+	previously_dodged = false
