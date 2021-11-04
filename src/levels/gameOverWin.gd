12281949f314fe68ede842e5edf0dc938a0c9bec
Landon Hammond <lthammond@bsu.edu>
2021-11-04 03:44:25 -0400

    Add GameOver win and loss screens

---
+++ src/levels/gameOverWin.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_MainMenuButton_pressed():
+	if get_tree().change_scene("res://src/levels/mainMenu.tscn") != OK:
+		print ("An unexpected error occured while trying to switch to mainMenu scene")
