91a0a8e38a173a513f541c21c1b3b26ef4a51b09
Nate Slagter <nateslagter@gmail.com>
2021-11-04 04:03:21 -0400

    Fix style guide errors

--- src/levels/gameOverLoss.gd
+++ src/levels/gameOverLoss.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-
-func _on_MainMenuButton_pressed():
+func _on_MainMenuButton_pressed() -> void:
 	if get_tree().change_scene("res://src/levels/mainMenu.tscn") != OK:
 		print ("An unexpected error occured while trying to switch to mainMenu scene")
