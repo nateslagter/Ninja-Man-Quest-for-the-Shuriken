69027b91f0cf00e98b7f7eae2981d8da5ad458d2
Nate Slagter <nateslagter@gmail.com>
2021-11-03 23:18:52 -0400

    Create player auto attack

---
+++ src/actors/Enemy.gd
@@ -0,0 +1,28 @@
+class_name Enemy
+extends KinematicBody2D
+
+
+onready var initial_pos := Vector2(position.x,position.y)
+
+var velocity := Vector2(50,0)
+var switch_velocity = 50
+var health := 3
+
+
+
+
+func _physics_process(delta):
+	if position.x > initial_pos.x + 50:
+		switch_velocity *= -1
+		velocity = Vector2(switch_velocity, 0)
+	elif position.x < initial_pos.x - 50:
+		switch_velocity *= -1
+		velocity = Vector2(switch_velocity, 0)
+	var _ignored = move_and_slide(velocity,Vector2(0,0))
+
+
+func _on_Area2D_area_entered(area):
+	if area.name == "SwordHitbox":
+		health -= 1
+	if health == 0:
+		queue_free()
--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -8,27 +8,29 @@ const gravity := 700
 
 var velocity := Vector2()
 var jumping := false
+var attacking := false
 
 func get_input() -> void:
-	velocity.x = 0
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= run_speed
-		$Sprite.flip_h = true
-		if jumping == false:
-			$AnimationPlayer.play("Walk")
-	if Input.is_action_pressed("move_right"):
-		velocity.x += run_speed
-		$Sprite.flip_h = false
-		if jumping == false:
-			$AnimationPlayer.play("Walk")
-	if Input.is_action_just_pressed("jump"):
-		if jumping == false and is_on_floor():
-			jumping = true
-			$AnimationPlayer.play("Jump")
-			velocity.y += jump_speed
-	if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
-		if jumping == false:
-			$AnimationPlayer.play("Idle")
+	if !attacking:
+		velocity.x = 0
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= run_speed
+			$Sprite.flip_h = true
+			if jumping == false:
+				$AnimationPlayer.play("Walk")
+		if Input.is_action_pressed("move_right"):
+			velocity.x += run_speed
+			$Sprite.flip_h = false
+			if jumping == false:
+				$AnimationPlayer.play("Walk")
+		if Input.is_action_just_pressed("jump"):
+			if jumping == false and is_on_floor():
+				jumping = true
+				$AnimationPlayer.play("Jump")
+				velocity.y += jump_speed
+		if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
+			if jumping == false:
+				$AnimationPlayer.play("Idle")
 		
 func _physics_process(delta: float) -> void:
 	get_input()
@@ -38,9 +40,21 @@ func _physics_process(delta: float) -> void:
 		jumping = false
 
 
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(body : Node2D) -> void:
 	if body is Boss:
 		Globals.health -= 1
+		print("lost health")
 		if Globals.health == 0:
 			queue_free()
 			
+
+
+func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
+	if anim_name == "Attack":
+		$AnimationPlayer.play("Idle")
+		attacking = false
+
+
+func _on_AutoAttack_body_entered(body):
+	attacking = true
+	$AnimationPlayer.play("Attack")
