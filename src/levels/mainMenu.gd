bdd8d976dc1366ab1d92786c19bd61fdf6cfde3d
Nate Slagter <nateslagter@gmail.com>
2021-11-04 03:48:30 -0400

    Reset player health and score after new attempt at level

--- src/levels/mainMenu.gd
+++ src/levels/mainMenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
 func _on_PlayButton_pressed():
+	Globals.score = 0
+	Globals.health = 5
 	if get_tree().change_scene("res://src/levels/testLevel.tscn") != OK:
 		print ("An unexpected error occured while trying to switch to testLevel scene")
