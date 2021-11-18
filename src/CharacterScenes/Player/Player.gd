23cf2e37b08b5cbc205b07879827987d4cf168ee
Landon Hammond <lthammond@bsu.edu>
2021-11-18 14:40:53 -0500

    Implement scene switching animation

--- src/CharacterScenes/Player/Player.gd
+++ src/CharacterScenes/Player/Player.gd
@@ -19,14 +19,16 @@ func attack() -> void:
 func _on_Area2D_body_entered(body : Node2D) -> void:
 	if (body is Boss or body.name =="AttackHitbox" or body.name == "EnemyHitbox") and damageable:
 		Globals.health -= 1
-		damageable = false
-		$InvincibilityTimer.start()
-		emit_signal("player_hit",body)
-		if Globals.health == 0:
-			queue_free()
+		if Globals.health > 0:
+			damageable = false
+			$InvincibilityTimer.start()
+			emit_signal("player_hit",body)
+		elif Globals.health == 0:
+			$DamageAnimation.play("Dead")
+			$SceneTransition/AnimationPlayer.play("SweepIn")
+			yield($SceneTransition/AnimationPlayer, "animation_finished")
 			if get_tree().change_scene("res://src/LevelAssets/MenuScenes/gameOverLoss.tscn") != OK:
 				print ("An unexpected error occured while trying to switch to gameOverLoss scene")
-
 
 
 func _on_InvincibilityTimer_timeout() -> void:
--- src/LevelAssets/LevelOne/LevelOne.gd
+++ src/LevelAssets/LevelOne/LevelOne.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 
 func _ready():
-	pass # Replace with function body.
+	$SceneTransition/AnimationPlayer.play("SweepOut")
 
 
 func _on_EndOfLevel_area_entered(_area):
+	$SceneTransition/AnimationPlayer.play("SweepIn")
+	yield($SceneTransition/AnimationPlayer, "animation_finished")
 	if get_tree().change_scene("res://src/LevelAssets/MenuScenes/gameOverWin.tscn") != OK:
 			print ("An unexpected error occured while trying to switch to gameOverWin scene")
