e63527280fd89ea4d2dba32107da1c3e06152d1c
Nate Slagter <nateslagter@gmail.com>
2021-11-19 15:57:54 -0500

    Rename levels folder

--- src/LevelScenes/LevelOne/LevelOne.gd
+++ src/LevelScenes/LevelOne/LevelOne.gd
@@ -4,5 +4,5 @@ extends Node2D
 func _on_LevelEnd_area_entered(_area):
 	$SceneTransition/AnimationPlayer.play("SweepIn")
 	yield($SceneTransition/AnimationPlayer, "animation_finished")
-	if get_tree().change_scene("res://src/LevelAssets/MenuScenes/gameOverWin.tscn") != OK:
+	if get_tree().change_scene("res://src/LevelScenes/MenuScenes/gameOverWin.tscn") != OK:
 			print ("An unexpected error occured while trying to switch to gameOverWin scene")
--- src/LevelScenes/MenuScenes/gameOverWin.gd
+++ src/LevelScenes/MenuScenes/gameOverWin.gd
@@ -3,5 +3,5 @@ extends Control
 func _on_MainMenuButton_pressed() -> void:
 	$SceneTransition/AnimationPlayer.play("SweepIn")
 	yield($SceneTransition/AnimationPlayer, "animation_finished")
-	if get_tree().change_scene("res://src/LevelAssets/MenuScenes//mainMenu.tscn") != OK:
+	if get_tree().change_scene("res://src/LevelScenes/MenuScenes//mainMenu.tscn") != OK:
 		print ("An unexpected error occured while trying to switch to mainMenu scene")
