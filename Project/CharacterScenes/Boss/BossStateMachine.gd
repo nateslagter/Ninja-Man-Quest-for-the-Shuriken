5456055bd7eb3a0ef6f7f6e648e39633e6ae01ca
Nate Slagter <nateslagter@gmail.com>
2021-12-05 20:48:58 -0500

    Fix issue where Raycast wasn't detecting Area2Ds

--- Project/CharacterScenes/Boss/BossStateMachine.gd
+++ Project/CharacterScenes/Boss/BossStateMachine.gd
@@ -1,16 +1,41 @@
-extends Node
+extends "res://Project/StateMachineInterface/State.gd"
 
+onready var animation_player = get_node("../AnimationPlayer")
+onready var boss_boundary_detector = get_node("../BossBoundaryDetector")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2(100,0)
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	call_deferred("set_state",States.IDLE)
+
+func _logic(_delta : float) -> void:
+	print(boss_boundary_detector.is_colliding())
+	if boss_boundary_detector.is_colliding():
+		if boss_boundary_detector.get_collider().is_in_group("BossBoundaries"):
+			print("detected")
+		else:
+			print("colliding, not detecting area")
+	parent.position.x += 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _transition(_delta : float):
+	return null
+	
+	
+func _enter_state(_new_state) -> void:
+	match state:
+		States.IDLE:
+			animation_player.play("Idle")
+		States.ATTACKING:
+			animation_player.play("Attack")
+		States.RUNNING:
+			animation_player.play("Walk")
+			
+	
+	
+func _exit_state(_new_state) -> void:
+	pass
+
+func _on_WaitTimer_timeout() -> void:
+	state = States.RUNNING
+	_enter_state(state)
